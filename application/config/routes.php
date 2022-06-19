2afe2d404993763f3319ad64e5a77c4cc325286a
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-06-19 16:17:53 +0200

    configuring the routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,4 +52,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'dashboard';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-// $route['products']='products/index';
+$route['products']='products/index';
