aafb89168526e21578f73d8ba006ab5315d69f37
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-06-19 17:09:10 +0200

    StockModel Codes

---
+++ application/models/StockModel.php
@@ -0,0 +1,38 @@
+<?php
+class StockModel extends CI_Model {
+    public function __construct(){
+        $this->load->database();
+    }
+    public function getInventory(){
+        $query = $this->db->get('stk_inventory');
+        return $query->result_array();
+    }
+    public function AddInventory(){
+        $data = array(
+            'quantity'=>$this->input->post('quantity'),
+            'productId'=>$this->input->post('productId')
+        );
+        return $this->db->insert('stk_inventory', $data);
+    }
+    public function UpdateInv($id){
+        $data = array(
+            'quantity'=>$this->input->post('quantity')        );
+        $this->db->where('inventory_id',$id);
+        return $this->db->update('stk_inventory',$data);
+        
+    }
+    public function getEachInv($id){
+        $this->db->select('*');
+        $this->db->from('stk_inventory');
+        $this->db->where('inventory_id',$id);
+        $objectQuery = $this->db->get();
+        return $objectQuery->result_array();
+    }
+    public function DeleteProduct($id){
+        $delete = $this->db->delete('stk_inventory', array('inventory_id'=>$id));
+        if($delete){
+            return true;
+        };
+    }
+}
+?>
