8a231e5cac3dd164659f070baf257ed89aa962e6
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-06-21 11:06:22 +0200

    Updating the form action and edit user

--- application/views/user/editUser.php
+++ application/views/user/editUser.php
@@ -67,7 +67,7 @@ input[type="text"]:focus,input[type="number"]:focus,input[type="file"]:focus{
         <div>
             <h1>Update User</h1>
         </div>
-        <form action="<?= base_url().'index.php/Dashboard/updateUser/'.$registered_details[0]['userId'] ?>" method="post">
+        <form action="<?= base_url('users/updateUser/').$registered_details[0]['userId'] ?>" method="post">
             <label>First Name</label>
             <input type="text" name="firstName" placeholder="Add Name" value="<?= $registered_details[0]['firstName']?>">
             <label>Last Name</label>
